44d3da65d751386182b02add47b2c49ae7a3fd34
Pahud <pahudnet@gmail.com>
2016-02-05 01:12:29 +0800

    support ${NGINX_CONF_URL} now

--- startup.sh
+++ startup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+if [[ ! -z ${NGINX_CONF_URL} ]]; then
+  wget ${NGINX_CONF_URL} -O /opt/openresty/nginx/conf/nginx.conf
+fi
+
 if [[ ! -z ${EXTRA_NGINX_CONF_URL} ]]; then
   wget ${EXTRA_NGINX_CONF_URL} -O /opt/openresty/nginx/conf/extra-locations.d/extra.conf
 fi
 
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf --nodaemon
+
